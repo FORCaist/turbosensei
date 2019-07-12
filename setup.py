dfc6bffe5f5201052d5df3559b5fcef9a2015db5
FORCaist <44247029+FORCaist@users.noreply.github.com>
2019-07-12 15:38:27 +1000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="turbosensei",
+    version="1.0.1",
+    author="FORCaist",
+    author_email="FORCaist.user@gmail.com",
+    description="The FORCsensei package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FORCaist",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
